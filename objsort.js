0e2e71c767b5dfba4002c6407d994772ca7e9497
Dineshambrose10 <vdineshambrose@gmail.com>
2025-06-12 12:29:46 +0530

    object sorting method

---
+++ objsort.js
@@ -0,0 +1,11 @@
+let a = { 
+    name : "dinesh",
+    age : 28,
+    adress : "chennai"
+}
+let b = Object.keys(a).sort()
+let c = Object.values(a).sort()
+let d = Object.entries(a).sort()
+console.log(b)
+console.log(c)
+console.log(d)
